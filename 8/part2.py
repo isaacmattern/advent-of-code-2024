fadc9393e5f0fe80685df928cfaa5e4d7e698eee
Isaac Mattern <isaac.mattern@gmail.com>
2024-12-18 15:36:23 -0500

    day 8

---
+++ 8/part2.py
@@ -0,0 +1,56 @@
+import collections
+
+def inBounds(point: tuple[int], height: int, width: int) -> bool:
+  if point[0] < 0 or point[0] >= height:
+    return False
+  if point[1] < 0 or point[1] >= width:
+    return False
+  
+  return True
+
+def solve():
+
+  input = open('input.txt', 'r')
+  
+  grid = []
+  for line in input:
+    trimmedLine = line.removesuffix('\n')
+    grid.append(trimmedLine)
+    
+  height = len(grid)
+  width = len(grid[0])
+  
+  antennaLists = collections.defaultdict(list)
+  
+  for i in range(height):
+    for j in range(width):
+      character = grid[i][j]
+      if character != '.':
+        antennaLists[character].append((i, j))
+  
+  # overlapping antinodes only count as one 'space' in the answer:
+  # "How many unique locations within the bounds of the map contain an antinode?"
+  antinodeSet = set()
+  for character in antennaLists.keys():
+    antennas = antennaLists[character]
+    for a in range(len(antennas) - 1):
+      for b in range(len(antennas)):
+        if a != b:
+          antenna1 = antennas[a]
+          antenna2 = antennas[b]
+          
+          diff = (antenna2[0] - antenna1[0], antenna2[1] - antenna1[1])
+          
+          antinode1 = antenna2
+          while inBounds(antinode1, height, width):
+            antinodeSet.add(antinode1)
+            antinode1 = (antinode1[0] + diff[0], antinode1[1] + diff[1])
+          
+          antinode2 = antenna1
+          while inBounds(antinode2, height, width):
+            antinodeSet.add(antinode2)
+            antinode2 = (antinode2[0] - diff[0], antinode2[1] - diff[1])
+
+  print(len(antinodeSet))      
+
+solve()
